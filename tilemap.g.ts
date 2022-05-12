baca0d9f06ffb099795b39b67ab1e3ada667702e
kimhuy1303 <93904045+kimhuy1303@users.noreply.github.com>
2022-05-12 18:16:23 +0700

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,102 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile15 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile17 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile18 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`10001000050505050504040405100105010a0b010202110205040404050101050e0f0c0e02020202050404010501010509020d0405050202050404040501010509020d0403050202050505130501010509020d0403050202020202020201010509020d0103050202020202020201010509020d0103050202020202020201010509020d0101050202050505050505050509020d0101050202050303030104030809020d0101050607051212121212120809020d010105020205050505050502020202020501050202020202020202020202020202040502020202020202020202020202020405050505050505050505050505050501040403040404030303030303030303`, img`
+. . . . . . . . 2 . . 2 2 2 2 2 
+. . . . 2 . . . 2 . . 2 . . . 2 
+. . . . 2 . 2 2 2 . . 2 . . . 2 
+2 2 . . 2 2 . . . . . 2 . . . 2 
+. 2 . . . . . . . . . 2 . . . 2 
+. 2 . . . . . . . . . 2 . . . 2 
+. 2 . . . . . . . . . 2 . . . 2 
+. 2 . . . . . . . . . 2 . . . 2 
+. 2 . . 2 2 2 2 2 2 2 2 . . . 2 
+. 2 . . 2 2 2 2 2 2 2 2 . . . 2 
+. 2 . . 2 2 2 2 2 2 2 2 . . . 2 
+. 2 . . 2 . . . . . . . . . . 2 
+. 2 . . . . . . . . . . . . . . 
+. 2 . . . . . . . . . . . . . . 
+. 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+. . . . . . . . 2 . . 2 . . . . 
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.castle.tilePath5,sprites.castle.tileGrass1,sprites.castle.tileGrass3,sprites.builtin.forestTiles0,myTiles.tile6,myTiles.tile7,sprites.builtin.forestTiles8,sprites.castle.tilePath4,myTiles.tile9,myTiles.tile11,myTiles.tile12,sprites.castle.tilePath6,myTiles.tile13,myTiles.tile14,myTiles.tile5,sprites.dungeon.chestClosed,myTiles.tile4,myTiles.tile18], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile3":
+            case "tile5":return tile5;
+            case "tile6":return tile6;
+            case "tile7":return tile7;
+            case "tile9":return tile9;
+            case "tile10":return tile10;
+            case "tile11":return tile11;
+            case "tile13":return tile13;
+            case "tile14":return tile14;
+            case "tile8":return tile8;
+            case "tile12":return tile12;
+            case "myTile4":
+            case "tile15":return tile15;
+            case "myTile5":
+            case "tile16":return tile16;
+            case "myTile6":
+            case "tile17":return tile17;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile7":
+            case "tile18":return tile18;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
